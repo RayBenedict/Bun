037e6b51b02af736c22ed4609cc1deb0e15bc600
RayBenedict <gorgonioraybenedict@gmail.com>
2023-10-01 22:01:28 +0800

    Initial commit bun

---
+++ modules.ts
@@ -0,0 +1,9 @@
+// import { file } from 'bun';
+// import path from 'path'
+const path = require('path');
+
+
+const filepath = path.join('foo', 'bar', 'image.png');
+const filename = path.basename(filepath);
+
+console.log(filename);
